400360cf504759333d3b97fedcf9f0a564a93390
aainstein <alejandrovip@gmail.com>
2017-06-08 17:33:54 -0300

    initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aainstein\appdata\local\programs\python\python36-32\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
